dcae9116b9a493d6ae199883a50e596a67339a6d
moodtrbl <sonickim6679@gmail.com>
2026-05-07 17:17:44 +0900

    fix: 3단계 람다 호출에 맞지 않는 권한 수정

--- terraform/environments/dev/training/scheduler.tf
+++ terraform/environments/dev/training/scheduler.tf
@@ -4,17 +4,3 @@ module "crawl_schedule" {
   schedule_expression = var.crawl_schedule_expression
   target_arn          = module.crawl_job.function_arn
 }
-
-module "preprocess_schedule" {
-  source              = "../../../modules/pipeline/eventbridge_scheduler"
-  name                = "${var.project_name}-${var.environment}-preprocess-schedule"
-  schedule_expression = var.preprocess_schedule_expression
-  target_arn          = module.preprocess_job.function_arn
-}
-
-module "augment_schedule" {
-  source              = "../../../modules/pipeline/eventbridge_scheduler"
-  name                = "${var.project_name}-${var.environment}-augment-schedule"
-  schedule_expression = var.augment_schedule_expression
-  target_arn          = module.augment_job.function_arn
-}
