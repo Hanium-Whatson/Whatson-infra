9b022b2a36001ce210205e7a932080a799f25974
anozanami <maguri02@naver.com>
2026-05-19 18:51:02 +0900

    [#22]; chore: storage.tf 포맷 재정렬

--- terraform/environments/dev/training/storage.tf
+++ terraform/environments/dev/training/storage.tf
@@ -3,7 +3,7 @@ data "aws_caller_identity" "current" {}
 data "aws_partition" "current" {}
 
 locals {
-  data_lake_bucket_arn       = "arn:${data.aws_partition.current.partition}:s3:::${var.data_lake_bucket_name}"
-  duplicate_guard_table_arn  = "arn:${data.aws_partition.current.partition}:dynamodb:${var.aws_region}:${data.aws_caller_identity.current.account_id}:table/${local.duplicate_guard_table_name}"
-  crawl_dlq_arn              = var.crawl_dlq_arn != "" ? var.crawl_dlq_arn : "arn:${data.aws_partition.current.partition}:sqs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:${local.crawl_dlq_name}"
+  data_lake_bucket_arn      = "arn:${data.aws_partition.current.partition}:s3:::${var.data_lake_bucket_name}"
+  duplicate_guard_table_arn = "arn:${data.aws_partition.current.partition}:dynamodb:${var.aws_region}:${data.aws_caller_identity.current.account_id}:table/${local.duplicate_guard_table_name}"
+  crawl_dlq_arn             = var.crawl_dlq_arn != "" ? var.crawl_dlq_arn : "arn:${data.aws_partition.current.partition}:sqs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:${local.crawl_dlq_name}"
 }
